3f2ab9026abc395e913b645a60a5b381a27072fe
sara-kassani <kassani.sara@gmail.com>
2018-05-08 16:09:14 -0600

    Add files via upload

---
+++ 06.template.intro.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, request, render_template
+app= Flask(__name__)
+
+
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name= None):
+    return render_template('hello.html', name_template= name)
+
+
+if __name__ == '__main__':
+    host= os.getenv('IP', '0.0.0.0')
+    port= int(os.getenv('PORT', 5000))
+    app.debug= True
+    app.run(host=host, port=port)
