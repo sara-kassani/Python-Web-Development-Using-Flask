856f746c9495b1c4ff1f4f71cdadcc4d58be0f72
sara-kassani <kassani.sara@gmail.com>
2018-05-09 13:07:06 -0600

    Add files via upload

---
+++ 08.login.function.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask, request, render_template
+app= Flask(__name__)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error= None
+    if request.method=='POST':
+        if valid_login(request.form['username'], request.form['password']):
+            return "Welcome back, %s" % request.form['username']
+        else:
+            error= "Incorrect username and password!"
+    return render_template('login-user.html', error= error)
+
+def valid_login(username, password):
+    if username == password:
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    host= os.getenv('IP', '0.0.0.0')
+    port= int(os.getenv('PORT', 5000))
+    app.debug= True
+    app.run(host=host, port=port)
